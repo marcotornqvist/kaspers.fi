fa8d1b7f82380d050b5daeb652a8c1249a7d1c91
Marco Törnqvist <marco.tornqvist@gmail.com>
2024-01-31 22:49:15 +0200

    remove gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,14 +15,6 @@ const config: Config = {
       "dark-yellow": "#E49C1C",
       beige: "#FCF5E8",
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "gradient-bg": "linear-gradient(180deg, #E49C1C, #FCF5E8)",
-      },
-    },
   },
   plugins: [],
 };
